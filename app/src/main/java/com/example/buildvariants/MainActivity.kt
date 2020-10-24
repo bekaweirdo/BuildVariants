f75a212e934eda54127f810cbfc8512095987ea0
bekachachua <beka.chachua@space.ge>
2020-10-24 15:05:44 +0400

    add test strings for build variants

--- app/src/main/java/com/example/buildvariants/MainActivity.kt
+++ app/src/main/java/com/example/buildvariants/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.buildvariants
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.text = getString(R.string.client_name)
     }
 }
